972427917c41c63a401268b24acb8508b50d787d
kvl00l <g12460766@tdrive.thu.edu.tw>
2024-04-22 11:48:53 +0800

    Revised

--- Calculator Lab2.py
+++ Calculator Lab2.py
@@ -3,7 +3,7 @@
 # Calculator Lab2
 #
 
-# 1. Function
+# Define Function
 def cal(x1, x2, op):
     if op == '+':
         return int(x1) + int(x2)
@@ -14,11 +14,13 @@ def cal(x1, x2, op):
     elif op == '/':
         return int(x1) / int(x2)
 
-# 2.Input
+# 1.Input
 x1 = input("Type x1     : ") 
 x2 = input("Type x2     : ")
 op = input("Operator    : ")
 
-# 3. Output
+# 2. Process
 sum = cal(x1, x2, op)
+
+# 3. Output
 print(f"Sum         : {sum}")
