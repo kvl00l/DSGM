7abc08215cb160b9cf157a243b56e0e3a44628fa
kvl00l <g12460766@tdrive.thu.edu.tw>
2024-05-06 11:10:56 +0800

    New

---
+++ Pandasi/Trial panda.py
@@ -0,0 +1,44 @@
+#
+# Kevin
+# Pandas
+#
+
+# Diabetes
+import pandas as pd 
+df = pd.read_csv('D:\DATA Science\DSGM\Pandasi\diabetes.csv')
+
+# Inspect structure
+df.shape 
+df.info()
+
+# Add missing values
+df2 = df.copy()
+df2.loc[2:5,'Pregnancies'] = None
+
+# Print df2
+print(df2.head(10))
+
+# Add missing values
+df2.isnull().sum()
+df2.shape
+
+# Remove rows with missing value
+df3 = df2.copy()
+df3 = df3.dropna()
+
+# Remove rows with missing value
+df3.shape
+
+# Creating new columns based on other columns
+df2['Glucose_Insulin_Ratio'] = df2['Glucose']/df2['Insulin']
+df2.head()
+
+# Count by value
+df['Outcome'].value_counts()
+df['Outcome'].value_counts(sort=False)
+
+# By group
+df.groupby('Outcome').mean()
+
+# By group
+df.groupby(['Pregnancies', 'Outcome']).mean()
